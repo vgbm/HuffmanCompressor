5370cf2ef726adf84554c60a20dfffdd5e277b9d
root <root@localhost.localdomain>
2015-03-19 17:03:52 -0400

    fixed freq

--- HuffmanCompressor.java
+++ HuffmanCompressor.java
@@ -9,53 +9,23 @@ public class HuffmanCompressor {
 
 	public static void main(String[] args) {
 		
-		File inputFile = new File(args[0]);
+		huffmanCoder("test.txt","fill in later");
+		
+		/*File inputFile = new File(args[0]);
 		File outputFile = new File(args[1]);
 		
-		ArrayList<String> textLines = new ArrayList<String>();
-		int[] frequencies = new int[26];
-		
-		try {
-		
-			Scanner fileReader = new Scanner(inputFile);
-			
-			while(fileReader.hasNext()){
-				textLines.add(fileReader.nextLine());
-			}
-			fileReader.close();
-			
-		} catch (FileNotFoundException e) {
-			System.out.print("File could not be found");
-			e.printStackTrace();
-		}
-		
-		//printList(textLines);
-		frequencies = countFrequencies(textLines);
-		
-		/*for(int num:frequencies){
-			System.out.println(num);
-		}*/
+		 */
 		
 	}
 	
-	public static int[] countFrequencies(ArrayList<String> list){ //works I think
-		int[] frequencies = new int[26];
-				
-		for(String line:list){
-			line.toLowerCase();
-			
-			for(int i = 0; i < line.length(); i++){
-				if(Character.isAlphabetic(line.charAt(i)))
-					frequencies[line.charAt(i)-97]++;
-			}
-		}
+	public static String huffmanCoder(String inputFileName, String outputFileName){
 		
-		return frequencies;
-	}
-	
-	public static void printList(ArrayList<String> list){
-		for(String line:list){
-			System.out.println(line);
-		}
+		File inputFile = new File(inputFileName);
+		HuffmanTree tree = new HuffmanTree();
+		
+		tree.buildNodeList(inputFile);
+		tree.printList();
+		
+		return "";
 	}
 }
--- HuffmanTree.java
+++ HuffmanTree.java
@@ -1,12 +1,87 @@
 package HuffmanEncoder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class HuffmanTree {
+	
+	ArrayList<HuffmanNode> nodeList;
+	HuffmanNode root;
+	
+	public HuffmanTree(){
+		nodeList = new ArrayList<HuffmanNode>();
+		nodeList.add(new HuffmanNode(' ',0)); // initialize the node list with a character so that its size is >0
+		root = new HuffmanNode(null,-1);
+	}
+	
+	
+	
+	public void buildNodeList(File inputFile){
+		
+		try {
+			
+			boolean found;
+			String line="";
+			Scanner fileReader = new Scanner(inputFile);
+			
+			while(fileReader.hasNext()){
+			
+				line = fileReader.next().toLowerCase();
+				
+				for(int i = 0; i < line.length(); i++){
+					
+					found=false;
+					
+					for(int j = 0; j < nodeList.size(); j++){
+						
+						if(nodeList.get(j).inChar.equals((Character)line.charAt(i))){
+							nodeList.get(j).frequency++;
+							found=true;
+							break;
+						}	
+					}
+			
+					if(!found){
+						nodeList.add(new HuffmanNode(line.charAt(i),1));
+					}	
+				}	
+			}
+			
+			fileReader.close();
+			
+		} catch (FileNotFoundException e) {		
+			System.out.print("Input file could not be found");
+			e.printStackTrace();
+		}
+	}
+	
+	public void makeTree(){
+		
+	}
+	
+	public void printList(){
+		for(HuffmanNode node: nodeList){
+			System.out.println(node);
+		}
+	}
 	
 	public class HuffmanNode{
 		
-		char inChar;
+		Character inChar;
 		int frequency;
 		HuffmanNode left, right;
+		
+		public HuffmanNode(Character inChar, int frequency){
+			this.inChar = inChar;
+			this.frequency = frequency;
+			left = right = null;
+		}
+		
+		public String toString(){
+			return "char:\t"+inChar+"\tFrequency:\t"+frequency;
+		}
 		
 	}
 	
